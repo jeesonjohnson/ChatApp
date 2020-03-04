0ed2ef6743073d30d9bb1b780180656dff2b7593
SabreOfWinter <steadmanbailey@gmail.com>
2020-03-04 21:15:54 +0000

    Init folder

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
+    companies: { type: Array, required: true},
+    friends: { type: Array, required: true}
+});
+
+const User = module.exports  = mongoose.model("users", UserSchema)
