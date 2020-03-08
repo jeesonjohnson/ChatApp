8067e85de7050113abe804b33823ffb53dc595a5
SabreOfWinter <steadmanbailey@gmail.com>
2020-03-08 23:58:45 +0000

    Environment variables configured

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoUSER: "admin", 
-    mongoPASSWORD: "EHtmgyrjm4CyQNIb",
-    mongoURI: `mongodb+srv://admin:EHtmgyrjm4CyQNIb@cluster-chat-app-su2qq.mongodb.net/test?retryWrites=true&w=majority`
-};
