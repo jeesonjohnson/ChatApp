86520c3193e7037a99220f865d7aea7b9b86474f
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-07 15:05:22 +0100

    Updated Navbar
    Removed unused parts
    Added alts for icons
    Icon sizes and position readjusted

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,25 +5,19 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-fixed">
-        <ul id="dropdown1" class="dropdown-content">
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
-            <li class="divider"></li>
-            <li><a href="#!">three</a></li>
-        </ul>
         <nav>
-            <div class="nav-wrapper">
-                <img id="logo" class="brand-logo" src="logo.png" height="64" width="62" alt=""/>
-                <ul id="abc" class=" hide-on-med-and-down">
-                    <li><a href="sass.html">Why Oogwai?</a></li>
-                    <li><a href="sass.html">Pricing</a></li>
-                    <li><a href="sass.html">Resources</a></li>
+            <div id="nav-wrapper" class="nav-wrapper">
+                <img id="logo" class="brand-logo" src="logo.png" height="50" width="50" alt=""/>
+                <ul id="left-navbar-list" class=" hide-on-med-and-down">
+                    <li class="left-navbar-button"><a href="sass.html">Why Oogwai?</a></li>
+                    <li class="left-navbar-button"><a href="sass.html">Pricing</a></li>
+                    <li class="left-navbar-button"><a href="sass.html">Resources</a></li>
                 </ul>
                 
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="https://twitter.com/"><img src="icon_twitter.png"/></a></li>
-                    <li><a href="https://www.facebook.com/"><img src="icon_facebook.png"/></a></li>
-                    <li><a href="https://www.instagram.com/"><img src="icon_instagram.png"/></a></li>
+                <ul id="right-navbar-list" class="right hide-on-med-and-down">
+                    <li><a href="https://twitter.com/"><img class="social-media-icons" src="icon_twitter.png" alt="Twitter"/></a></li>
+                    <li><a href="https://www.facebook.com/"><img class="social-media-icons" src="icon_facebook.png" alt="Facebook"/></a></li>
+                    <li><a href="https://www.instagram.com/"><img class="social-media-icons" src="icon_instagram.png" alt="Instagram"/></a></li>
                     <li><a href="badges.html">Sign up</a></li>
                     <li><a href="badges.html">Login</a></li>
                 </ul>
