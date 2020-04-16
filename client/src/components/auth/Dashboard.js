6cf9f6854d5e51efb0c798fa38b511e97587004f
SabreOfWinter <steadmanbailey@gmail.com>
2020-04-17 00:28:39 +0100

    Created Dashboard

---
+++ client/src/components/auth/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import './Auth.css'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div id="dashboard" className="" style={{}}>
+        <div className="row" style={{marginTop:"10px", marginLeft:0, marginRight:0}} >
+          
+        </div>
+      </div>
+    );
+  }        
+}
+
+export default Dashboard;
