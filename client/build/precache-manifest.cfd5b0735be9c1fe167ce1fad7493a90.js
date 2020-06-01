de451952ef806c7910b7f904c8cea4088a57d90d
JeesonJohnson <jeesonjohnson@github.com>
2020-06-01 19:29:13 +0100

    test

---
+++ client/build/precache-manifest.cfd5b0735be9c1fe167ce1fad7493a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34300df3ab433c4f74e889080fede98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c6aa8d834871edc916",
+    "url": "/static/css/main.229bffaa.chunk.css"
+  },
+  {
+    "revision": "374423a9c9d418d5b85f",
+    "url": "/static/js/2.3936856a.chunk.js"
+  },
+  {
+    "revision": "924de7ddc62aeda9757da4c05e821dd5",
+    "url": "/static/js/2.3936856a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c6aa8d834871edc916",
+    "url": "/static/js/main.97bc36ad.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "d85aec3c25b8b09c81651acb324d7a4e",
+    "url": "/static/media/pinDrop.d85aec3c.png"
+  }
+]);
